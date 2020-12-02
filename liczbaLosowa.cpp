786cd669deaa42f5e4c4428fa4caed3fc767c6b0
piotr310100 <56043614+piotr310100@users.noreply.github.com>
2020-12-02 09:46:20 +0100

    Update liczbaLosowa.cpp

--- liczbaLosowa.cpp
+++ liczbaLosowa.cpp
@@ -7,5 +7,6 @@ int main(){
 	random_device r;
 	default_random_engine e1(r());
 	uniform_int_distribution<int> uniform_dist(1, 6);
-	return uniform_dist(e1);
+	cout << uniform_dist(e1) << endl;
+	return 0;
 }
